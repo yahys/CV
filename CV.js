2396e693729b4e166fad66c3c300e9ffd749fa8b
Yahya <73270815+yahys@users.noreply.github.com>
2024-10-24 18:40:39 +0200

    Update CV.js

--- CV.js
+++ CV.js
@@ -18,7 +18,7 @@ function CV() {
       missions: [
         {
           title: "Appui au support et évolutions du Treasury Management System - Titantreasury",
-          description: "dans le cadre des activités de la Société du Grand Paris et au vu de ses besoins de financement, a été déployé au sein de son SI le progiciel Titan pour la gestion de sa dette, du risque et de sa trésorerie. L’objectif étant d’accompagner le Client pour maintenir la solution (évolutions et correctifs)",
+          description: "Dans le cadre des activités de la Société du Grand Paris et au vu de ses besoins de financement, a été déployé au sein de son SI le progiciel Titan pour la gestion de sa dette, du risque et de sa trésorerie. L’objectif étant d’accompagner le Client pour maintenir la solution (évolutions et correctifs)",
           tasks: [
             "Cadrage des besoins",
             "Expression des besoin",
@@ -33,7 +33,6 @@ function CV() {
         {
           title: "Déploiement de SAP S4/HANA modules FM, CO/PS, MM, FI",
           description: "La SGP Dev a été créée afin de valoriser les savoirs et savoir-faire acquis dans la conception et la réalisation du Grand Paris Express. Dans le cadre de ses missions, la filiale de la SGP avait exprimé le besoin de se doter d’un outil d’appui à la gestion financière",
-
           tasks: [
             "Cadrage des besoins",
             "Pilotage des équipes transverses internes et externes",
@@ -46,14 +45,14 @@ function CV() {
         },
         {
           title: "Sécurisation de l'actuelle ERP Qualiac",
-          description : "en pleine migration d’outil de gestion entre Qualiac vers SAP S4/HANA, des solutions paliatives de secours doivent être déployées pour garantir la continuité de service opérationnelle jusqu’à la bascule",
+          description: "En pleine migration d’outil de gestion entre Qualiac vers SAP S4/HANA, des solutions paliatives de secours doivent être déployées pour garantir la continuité de service opérationnelle jusqu’à la bascule",
           tasks: [
             "Solutions paliatives de secours pour garantir la continuité de service"
           ]
         },
         {
-          title: "Plan de Continuité Métiers"
-          description : "déploiement d’un plan de gestion de crise en fonction de la criticité des flux des domaines Achats, Finance et Comptables pour permettre la continuité de service",
+          title: "Plan de Continuité Métiers",
+          description: "Déploiement d’un plan de gestion de crise en fonction de la criticité des flux des domaines Achats, Finance et Comptables pour permettre la continuité de service",
           tasks: [
             "Déploiement d'un plan de gestion de crise",
             "Analyse de la criticité des flux des domaines Achats, Finance et Comptables"
@@ -247,7 +246,7 @@ function CV() {
   return (
     <div className="max-w-4xl mx-auto p-8 bg-white">
       <header className="mb-8 flex items-center">
-        <img src="./images/IMG_5847.jpg" alt="Yahya BENABDELKRIM" className="w-32 h-32 rounded-full mr-8 object-cover" />
+        <img src="./IMG_5847.jpg" alt="Yahya BENABDELKRIM" className="w-32 h-32 rounded-full mr-8 object-cover" />
         <div>
           <h1 className="text-3xl font-bold mb-2">Yahya BENABDELKRIM</h1>
           <h2 className="text-xl text-gray-600 mb-4">Chef de projets AMOA spécialisé en SI Finance & Trésorerie</h2>
